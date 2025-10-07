98dc9c45a8cb2ece766ac71f8cb8dbb31350bb56
user.email like: hillmatthew2000@gmail.com <hillmatthew2000@gmail.com>
2025-10-07 12:08:29 -0500

    Add initial project files for Binary Search implementation

---
+++ Search/BinarySearch/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Globalization;
+
+namespace BinarySearch;
+
+class Program
+{
+    static void Main()
+    {
+        int[] orderIDs = { 1, 2, 3, 4, 5, 6, 7, 8 };
+        Console.WriteLine(BinarySearch(orderIDs, 3));
+
+    }
+
+    static int BinarySearch(int[] nums, int target)
+    {
+        int low = 0;
+        int high = nums.Length - 1;
+
+        while (low <= high)
+        {
+            int mid = (low + high) / 2;
+            int guess = nums[mid];
+
+            if (guess == target)
+                return mid;
+            if (guess < target)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+        return -1;
+    }
+
+   
+}
